80c8d1381477d3dbb96f9fc64a39f051127b302a
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-02-21 12:50:07 +0200

    Add Required Methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
     belongs_to :user
     belongs_to :post
+
+
+    def updateLikesCounter
+        post.increment!(:LikesCounter)
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
     belongs_to :user
     has_many :likes
     has_many :comments
+
+    def updatePostsCounter
+        post.increment!(:PostsCounter)
+    end
+
+    def five_recent_comments
+        comments.order(created_at: :desc).limit(5)
+    end
 end
