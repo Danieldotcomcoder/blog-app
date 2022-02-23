7f71e2112412418fc7e59d807f94b91c6982d81d
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-02-23 12:33:09 +0200

    Add Create Post Form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
  
   resources :users, only: [:show, :index] do
    resources :posts, only: [:show, :index]
+   resources :comments, only: [:create]
+   resources :likes, only: [:create]
   end
 
+  resources :posts, only: [:new, :create]
   root 'users#index'
 end
