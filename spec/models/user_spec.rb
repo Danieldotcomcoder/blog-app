fb4ce848431c709d40840732176ef9d34f56e4dd
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-02-24 11:28:25 +0200

    Add unit specs for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(Name: 'User1', Photo:'https://unsplash.com/photos/F_-0BxGuVvo', Bio:'Software developer from lebanon') }
+  
+  before {subject.save}
+
+  it 'Name Should be Present' do
+    subject.Name = nil 
+    expect(subject).to_not be_valid
+  end
+
+  it 'Photo should be a string' do
+    subject.Photo = 'photo source'
+    expect(subject).to be_valid
+  end
+
+  it 'Posts counter should be equal or greater than zero' do
+    subject.posts_counter = -1
+    expect(subject).to_not be_valid
+  end
+  
+  it 'recent posts method should return last three posts' do
+ post1 =  Post.create(user: subject, Title: 'One', Text: 'first post' )
+ post2 =  Post.create(user: subject, Title: 'Two', Text: 'second post' )
+ post3 =  Post.create(user: subject, Title: 'Three', Text: 'third post' )
+ post4 =  Post.create(user: subject, Title: 'Four', Text: 'fourth post' )
+ post5 =  Post.create(user: subject, Title: 'Five', Text: 'fifth post' )
+  expect(subject.recent_posts.length).to eq 3 
+  expect(subject.recent_posts).to include post5
+  expect(subject.recent_posts).to include post4
+  expect(subject.recent_posts).to include post3
+  end
+  
 end
