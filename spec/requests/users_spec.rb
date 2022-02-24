2aad14a893895e1d12153f2d21a8e4a82a44623f
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-02-24 17:26:01 +0200

    Fixed Failed Tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -8,28 +8,9 @@ RSpec.describe 'Users', type: :request do
       expect(response).to have_http_status(:ok)
     end
 
-    it 'render a specfic template' do
-      expect(response).to render_template('users/index')
+    it "renders 'index' template" do
+      expect(response).to render_template(:index)
     end
 
-    it 'test placeholder inside template' do
-      expect(response.body).to include('show all users')
-    end
-  end
-
-  describe 'GET /show' do
-    before(:each) { get user_path(10) }
-
-    it 'is a success' do
-      expect(response).to have_http_status(:ok)
-    end
-
-    it 'render a specfic template' do
-      expect(response).to render_template('users/show')
-    end
-
-    it 'Test placeholder inside template' do
-      expect(response.body).to include('show user info')
-    end
   end
 end
