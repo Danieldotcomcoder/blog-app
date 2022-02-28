630a77a961e47c8e6c92d5a498dce44e9f59de11
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-02-28 19:44:12 +0200

    Setup devise to send confirmation mails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
+ 
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts
   has_many :comments
   has_many :likes
 
-  validates :Name, presence: true
+  # validates :Name, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
   def recent_posts
     posts.order(created_at: :desc).limit(3)
