a91a8cc51211e55d9876d2ec2a7a4329a5fe698a
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-03-01 12:40:26 +0200

    Fix application controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  # validates :Name, presence: true
+  validates :Name, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
   def recent_posts
     posts.order(created_at: :desc).limit(3)
