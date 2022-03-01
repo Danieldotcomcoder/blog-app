93cb33438ad8bada888e28d9acad05dc214aa8d5
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-03-01 14:32:04 +0200

    Remove credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -80,13 +80,5 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    user_name:      'danielshdeed1995.ds@gmail.com',
-    password:       'danielshdeed123!@#',
-    domain:         'localhost:3000',
-    address:       'smtp.gmail.com',
-    port:          '587',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+ 
 end
