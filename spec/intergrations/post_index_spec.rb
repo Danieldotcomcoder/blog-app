7c0be69f6d7c8175eeac89bd677ec15c2fe69733
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-03-03 17:51:08 +0200

    Fix linter errors

--- spec/intergrations/post_index_spec.rb
+++ spec/intergrations/post_index_spec.rb
@@ -3,41 +3,38 @@ require 'rails_helper'
 RSpec.describe 'On Post Index Page', type: :feature do
   before(:each) do
     visit user_session_path
-    @Danny= User.create!(Name: 'Dan',
-      email: 'myemail12345@gmail.com',
-      Bio: 'bla',
-      password: 'password',
-      password_confirmation: 'password',
-      posts_counter: 0,
-      confirmed_at: Time.now)
-      
+    @danny = User.create!(Name: 'Dan',
+                          email: 'myemail12345@gmail.com',
+                          Bio: 'bla',
+                          password: 'password',
+                          password_confirmation: 'password',
+                          posts_counter: 0,
+                          confirmed_at: Time.now)
 
     within('#new_user') do
       fill_in 'Email', with: 'myemail12345@gmail.com'
       fill_in 'Password', with: 'password'
     end
     click_button 'Log in'
-    @post1 = @Danny.posts.create!(Title: 'post 1', Text: 'My post 1', user_id: @Danny.id, comments_counter: 0,
-      likes_counter: 0)
-    @post2 = @Danny.posts.create!(Title: 'post 2', Text: 'My post 2', user_id: @Danny.id, comments_counter: 0,
-      likes_counter: 0)
-    @post3 = @Danny.posts.create!(Title: 'post 3', Text: 'My post 3', user_id: @Danny.id, comments_counter: 0,
-      likes_counter: 0)
-   
+    @post1 = @danny.posts.create!(Title: 'post 1', Text: 'My post 1', user_id: @danny.id, comments_counter: 0,
+                                  likes_counter: 0)
+    @post2 = @danny.posts.create!(Title: 'post 2', Text: 'My post 2', user_id: @danny.id, comments_counter: 0,
+                                  likes_counter: 0)
+    @post3 = @danny.posts.create!(Title: 'post 3', Text: 'My post 3', user_id: @danny.id, comments_counter: 0,
+                                  likes_counter: 0)
 
-    @Sam = User.create!(
+    @sam = User.create!(
       Name: 'Sam', Bio: 'daibda', email: 'sam@gmail.com',
-      password: 'sam1234',password_confirmation: 'sam1234', confirmed_at: Time.now
+      password: 'sam1234', password_confirmation: 'sam1234', confirmed_at: Time.now
     )
-    @Sam.comments.create!(Text: 'Nice', post: @post1)
-    @Sam.comments.create!(Text: 'Great', post: @post1)
-    @Sam.comments.create!(Text: 'Cool', post: @post1)
-   
+    @sam.comments.create!(Text: 'Nice', post: @post1)
+    @sam.comments.create!(Text: 'Great', post: @post1)
+    @sam.comments.create!(Text: 'Cool', post: @post1)
+
     visit user_posts_path(@Danny)
   end
 
   describe 'post' do
-  
     it 'test username' do
       expect(page).to have_content 'Dan'
     end
